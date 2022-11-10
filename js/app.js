c2e6049a9ccefea45710bb59076fdbd47df2c66d
8Tanya28 <4090535@gmail.com>
2022-11-10 11:59:15 +0200

    fin1

--- js/app.js
+++ js/app.js
@@ -1,65 +1,79 @@
-const galleryItems = [
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
-      description: 'Hokkaido Flower',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
-      description: 'Container Haulage Freight',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
-      description: 'Aerial Beach View',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
-      description: 'Flower Blooms',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
-      description: 'Alpine Mountains',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
-      description: 'Mountain Lake Sailing',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
-      description: 'Alpine Spring Meadows',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
-      description: 'Nature Landscape',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
-      description: 'Lighthouse Coast Sea',
-    },
-  ];
+import images from "./galleryItems.js";
+
+const galleryContainer = document.querySelector(".js-gallery");
+const imagesMarkup = createGalleryItemsMarkup(images);
+const lightBoxImage = document.querySelector(".js-lightbox");
+const imageItem = document.querySelector(".lightbox__image");
+const backdrop = document.querySelector(".lightbox__overlay");
+const btnClose = document.querySelector('[data-action="close-lightbox"]');
+
+galleryContainer.insertAdjacentHTML("beforeend", imagesMarkup);
+galleryContainer.addEventListener("click", onGalleryItemClick);
+btnClose.addEventListener("click", onHandlerClose);
+backdrop.addEventListener("click", onBackdropClick);
+// - Создание и рендер разметки по массиву данных `galleryItems` из `app.js` и
+//   предоставленному шаблону.
+function createGalleryItemsMarkup(images) {
+  return images
+    .map(({ preview, description, original }) => {
+      return `<li class="gallery__item">
+  <a
+    class="gallery__link"
+    href=${original}
+  >
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</li>`;
+    })
+    .join("");
+}
+// - Реализация делегирования на галерее `ul.js-gallery` и получение `url` большого
+//   изображения.
+function onGalleryItemClick(e) {
+  e.preventDefault();
+  const isGalleryImageEl = e.target.classList.contains("gallery__image");
+  if (!isGalleryImageEl) {
+    return;
+  }
+  // - Открытие модального окна по клику на элементе галереи.
+  if (isGalleryImageEl) {
+    window.addEventListener("keydown", onEscKeyPress);
+    lightBoxImage.classList.add("is-open");
+
+    // - Подмена значения атрибута `src` элемента `img.lightbox__image`.
+    imageItem.src = e.target.getAttribute("data-source");
+    imageItem.alt = e.target.alt;
+  }
+}
+
+// - Закрытие модального окна по клику на кнопку
+//   `button[data-action="close-lightbox"]`.
+function onHandlerClose(e) {
+  // e.preventDefault();
+  window.removeEventListener("keydown", onEscKeyPress);
+  lightBoxImage.classList.remove("is-open");
+  // - Очистка значения атрибута `src` элемента `img.lightbox__image`. Это необходимо
+  //   для того, чтобы при следующем открытии модального окна, пока грузится
+  //   изображение, мы не видели предыдущее.
+  imageItem.src = "";
+  imageItem.alt = "";
+}
+// Закрытие модального окна по клику на `div.lightbox__overlay`
+function onBackdropClick(e) {
+  if (e.target === e.currentTarget) {
+    onHandlerClose();
+  }
+}
+// Закрытие модального окна по нажатию клавиши `ESC`
+function onEscKeyPress(e) {
+  console.log(e);
+  const ESC_KEY_CODE = "Escape";
+  if (e.code === ESC_KEY_CODE) {
+    onHandlerClose();
+  }
+}
